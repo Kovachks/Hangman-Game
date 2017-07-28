f1eb05e6bfeb1bb96dc6d10b465b5173f697e32e
unknown <kovachks90@gmail.comgit config --global kovachks90 Keith>
2017-07-27 20:50:02 -0400

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 /*
 Generate a word to be used in the game - Done
-display the length of the word with hidden letters
+display the length of the word with hidden letters - Done
 create onkeyup event and store value for user input
 check user guess against array of letters
 if the guess was correct display letters
@@ -22,7 +22,9 @@ var losses = 0;
 var guessesLeft = 12;
 
 //word bank
-var wordBank = ["hornets", "cavaliers", "celtics", "lakers", "rockets", "thunder"];
+var wordBank = ["hornets", "cavaliers", "celtics", "lakers", "rockets", "thunder", "pacers", "bulls", "magic", "jazz", "clippers", "spurs", "raptors"];
+
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // computer selection from aray
 var computerSelection = undefined;
@@ -30,6 +32,7 @@ var computerSelection = undefined;
 //container for split guess
 var computerSplit = undefined;
 
+//array for holding user guesses
 var userGuesses = [];
 
 var computerLetter = $("#computerLetter");
@@ -41,6 +44,8 @@ function computerGuessGen() {
 	return Math.floor(Math.random() * wordBank.length);
 }
 
+
+
 //Storing randomly generated computer word as variable
 computerSelection = wordBank[computerGuessGen()];
 
@@ -49,13 +54,18 @@ computerSplit = computerSelection.split("");
 console.log(computerSplit);
 
 //Creating letter container for computer generated word
-$("#computerWord").html("Test");
+$("#computerWord").html("");
 for (var i = 0; i < computerSplit.length; i += 1) {
-	var newLetterDiv = $("<div>" + computerSplit[i] + "</div>");
+	var newLetterDiv = $("<div></div>");
+	newLetterDiv.addClass("container");
 	computerLetter.append(newLetterDiv);
 }
-console.log(newLetterDiv);
+
+//Creating onkeyup event to log the users keystroke
+document.onkeyup = function(event) {
+	var userGuess = event.key;}
 
 //for each letter create a letter container.  add text html to the letter container.
 //after letters are completely filled out add that container to the above nested container.
 //for underscores divs with a height of 5 pixels and width is the same as the letter
+console.log(userGuess());
